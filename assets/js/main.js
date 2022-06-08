5636a364a3d9150cd73d554f660aeab727b376a5
chmery <104914995+chmery@users.noreply.github.com>
2022-06-08 11:10:51 +0200

    Update main script

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,19 @@ const hintOutput = document.querySelector('.container__text-hint');
 const hintBtn = document.querySelector('#get-hint');
 const typeContainer = document.querySelector('.container__type');
 const typeOutput = document.querySelector('#type');
+const attemptOutput = document.querySelector('#attempts');
 const input = document.querySelector('.container__input');
 
-const randomItem = Math.floor(Math.random()*passwords.length);
+let randomItem = Math.floor(Math.random()*passwords.length);
 
+let attemptCount = 1;
 let hintArray = 0;
-hintOutput.innerHTML = passwords[randomItem].hints[hintArray];
-
 let hintCount = hintArray + 1;
+
+hintOutput.innerHTML = passwords[randomItem].hints[hintArray];
 hintNumber.innerHTML = "Hint " + hintCount;
 
-let typeChecker = (randomItem) => {
+const typeChecker = (randomItem) => {
     if (passwords[randomItem].type == "easy"){
         typeContainer.setAttribute('class', 'container__type container__type--easy')
         typeOutput.innerHTML = "Easy";
@@ -24,39 +26,45 @@ let typeChecker = (randomItem) => {
         typeContainer.setAttribute('class', 'container__type container__type--hard')
         typeOutput.innerHTML = "Hard";
     }
-
-    console.log('test')
 }
 
 typeChecker(randomItem);
 
-const hints = () => {
-    if(input.value == passwords[randomItem].keyword){
-        alert("Good answer")
-    }
-    
-    if (input.value != passwords[randomItem].keyword){
+const reset = () => {
+    randomItem = Math.floor(Math.random()*passwords.length);
+    typeChecker(randomItem)
+    hintCount = 1;
+    hintArray = 0;
+    attemptCount = 1;
+    hintOutput.innerHTML = passwords[randomItem].hints[hintArray];
+    hintNumber.innerHTML = "Hint " + hintCount;
+    attemptOutput.innerHTML = attemptCount
+}
+
+const getHint = () => {
+    if(hintArray < 4) {
         hintArray++
         hintCount++
         hintOutput.innerHTML = passwords[randomItem].hints[hintArray];
         hintNumber.innerHTML = "Hint " + hintCount;
-    } 
+        if (passwords[randomItem].type == "easy"){
+            attemptCount += 6
+            attemptOutput.innerHTML = attemptCount
+        } else if (passwords[randomItem].type == "medium"){
+            attemptCount += 4
+            attemptOutput.innerHTML = attemptCount
+        } else {
+            attemptCount += 2
+            attemptOutput.innerHTML = attemptCount
+        }
+    }
 
-    if (input.value != passwords[randomItem].keyword && hintArray >= 5) {
-        alert("Hint limit")
-        resetHints()
-        resetTimer()
+    if (hintArray == 4) {
+        alert("This is your last hint. You have to guess now!")
     }
 }
 
-const resetHints = () => {
-    const randomItem = Math.floor(Math.random()*passwords.length);
-    typeChecker(randomItem)
-    hintCount = 1;
-    hintArray = 0;
-    hintOutput.innerHTML = passwords[randomItem].hints[hintArray];
-    hintNumber.innerHTML = "Hint " + hintCount;
-}
+hintBtn.addEventListener('click', getHint)
+resetBtn.addEventListener('click', reset)
 
-hintBtn.addEventListener('click', hints)
-resetBtn.addEventListener('click', resetHints)
+
